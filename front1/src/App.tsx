5f2bdc5ad14e065bb1338c9c09bb0996f74d1742
ale3serper <ale3serper@gmail.com>
2026-02-18 23:33:12 +0100

    rick&morty

--- front1/src/App.tsx
+++ front1/src/App.tsx
@@ -1,35 +1,62 @@
-import { useEffect, useState } from 'react'
+
+import { useEffect, useState } from "react";
 import './App.css'
-import { api } from './api/api'
-import { Character } from './components'
-import type { CharacterT } from './types'
+import { Character } from "./components/index";
+import type { CharacterT } from "./types";
+import { api } from "./api/api";
+
+const App = () => {
+  const [characters, setCharacters] = useState<CharacterT[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [name, setName] = useState<string>("");
+  const [finalName, setFinalName] = useState<string>("");
 
 
+  const fetchCharacters = async (name: string) => {
+    setLoading(true);
+    setError(null);
+    await api
+      .get(`/character/${name ? "?name=" + name : ""}`)
+      .then((e) => setCharacters(e.data.results))
+      .catch((e) => {
+        setError(`Error al obtener los datos: ${e}`);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
+  useEffect(() => {
+    fetchCharacters(finalName);
+  }, [finalName]);
 
+  return (
+    <>
+      <input
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setFinalName(name);
+        }}
+      >
+        Search
+      </button>
+      {loading && <h2>Loading...</h2>}
+      {error && <h3>Error: {error}</h3>}
+      {!loading && (
+        <div className="charactersGrid">
+          {characters.map((e) => (
+            <Character key={e.id} character={e} />
+       ))}
+  </div>
+)}
+    </>
+  );
+};
 
-const  App = () => {
-
-  const [search,setSearch] = useState<string>("");
-  const [inputText,setInputText] = useState<string>("")
-  const [characters,setCharacters] = useState<CharacterT[]>([]);
-  
-    useEffect(() => {
-    search && api.get(`characterName?name=${search}`).then(e=>{
-      setCharacters(e.data.results)})
-    },[search])
-  
-
- return (
-    <div className='mainContainer'>
-      <input type="text" value={inputText} onChange={(e) => setInputText(e.target.value)}/>
-      <button onClick={() => setSearch(inputText)}> Search </button>
-      <div className='characterContainer'>
-        {characters.map((e) => <Character key={e.id} character={e}/>)}
-      </div>
-
-    </div>
-  )
-}
-
-export default App;
+export default App
--- front1/src/components/index.tsx
+++ front1/src/components/index.tsx
@@ -1,33 +1,33 @@
-import './style.css'
-import type { CharacterT } from '../types'
-import { useEffect, useState } from 'react'
-import { api } from '../api/api';
-// responsable de llamar a la api para recolectar toda la info del personaje
-export const Character=(params:{id?:string,character?:CharacterT})=>{
-// si me pasas el personaje completo, haz que sea el personaje ENTERO (por eso hacemos lo de despues de id y params)
+import { useEffect, useState } from "react";
+import type { CharacterT } from "../types";
+import { api } from "../api/api";
+import "./style.css";
+
+
+
+
+export const Character = (params: {id?: number, character?: CharacterT}) => {
+
+
     const id = params.id;
-    const paramCharacter = params.character
-    const [character,setCharacter] = useState<CharacterT|null>(paramCharacter ? paramCharacter:null);
-   
-    useEffect(()=>{
-            id && api.get(`/character/${id}`).then((res)=>{
-                setCharacter(res.data);
-            });
-    },[id]);
-    // no entendí lo del id, no tenían que ser State?
+
+    const [personajito, setPersonajito] = useState<CharacterT | null>(params.character ? params.character : null);
+
+
+    useEffect(() => {
+        !params.character && id && api.get(`/character/${id}`).then((e) => setPersonajito(e.data));
+    }, [id]);
 
     return(
         <>
-       {character ? (<div className="characterContainer">
-            <img src={character?.image}/>
-            <div className='characterDataContainer'>
-                <p>Nombre:{character?.name}</p>
-                <p>Origen:{character?.origin.name}</p>
-                <p>Estado:{character?.status}</p>
-            </div>
-        </div>) : (<h1>Loading...</h1>)}
+            {personajito ? <div className="mainContainer">
+                {personajito?.image && <img src={personajito?.image}/>}
+                <div className="characterDataContainer">
+                    <h2>{personajito?.name}</h2>
+                    <p>Origin: {personajito?.origin.name}</p>
+                    <p>Location: {personajito?.location.name}</p>
+                </div>
+            </div> : <h1>Loading...</h1>}
         </>
-        // los corchangulos vacios son como una especie de promesa de que lo del interior será html, ya que al hacer lo de character && y el div grande tenemos que hacer que existe el character
-
     )
 }
